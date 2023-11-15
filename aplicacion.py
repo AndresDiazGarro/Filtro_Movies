2ed8f360b5d38ab9d2182b830c86e782c3105964
kebelth <kebelthcalvo@gmail.com>
2023-11-14 20:05:51 -0600

    Ajustes
    modificaciones generales

--- aplicacion.py
+++ aplicacion.py
@@ -1,15 +1,17 @@
 import pickle
-import streamlit as st
+import textwrap
+
 import numpy as np
+import streamlit as st
 
 st.header("Sistema de recomendación de peliculas basado en filtro colavorativo")
-modelo = pickle.load(open('activos/model.pkl', 'rb'))
-nombres_peliculas = pickle.load(open('activos/peliculas_Nombre.pkl', 'rb'))
-clasificaciones_finales = pickle.load(open('activos/clasificaciones_Finales.pkl', 'rb'))
-peliculas_pivot = pickle.load(open('activos/peliculas_Tabla_Pivot.pkl', 'rb'))
+modelo = pickle.load(open(r'C:/Users/kebelth/Documents/Filtro_Movies/activos/model.pkl', 'rb'))
+nombres_peliculas = pickle.load(open(r'C:/Users/kebelth/Documents/Filtro_Movies/activos/peliculas_Nombre.pkl', 'rb'))
+clasificaciones_finales = pickle.load(open(r'C:/Users/kebelth/Documents/Filtro_Movies/activos/clasificaciones_Finales.pkl', 'rb'))
+peliculas_pivot = pickle.load(open(r'C:/Users/kebelth/Documents/Filtro_Movies/activos/peliculas_Tabla_Pivot.pkl', 'rb'))
 
 #Función para obtener la portada de la pelicula
-"""
+
 def conseguir_portada(sugerencia):
     nombre_pelicula = []
     id_indice = []
@@ -27,20 +29,19 @@ def conseguir_portada(sugerencia):
         url_portada.append(url)
         
     return url_portada
-"""
+
+
 #Función para recomendar pelicula de acuerdo al número de indice del nombre dado como parámetro
 
 def recomendar_Pelicula(nombre_Pelicula):
     lista_peliculas = []
     pelicula_id = np.where(peliculas_pivot.index == nombre_Pelicula)[0][0]
-    sugerencia = modelo.kneighbors(peliculas_pivot.iloc[pelicula_id,:].values.reshape(1,-1), n_neighbors=5)
+    distancias, indices_sugerencias = modelo.kneighbors(peliculas_pivot.iloc[pelicula_id,:].values.reshape(1,-1), n_neighbors=5)
 
-    #url_portada = conseguir_portada(sugerencia)
+    for i in indices_sugerencias[0]:
+        pelicula_sugerida = peliculas_pivot.index[i]
+        lista_peliculas.append(pelicula_sugerida)
 
-    for i in range(len(sugerencia)):
-        peliculas = peliculas_pivot.index[sugerencia[i]]
-        for j in peliculas:
-            lista_peliculas.append(j)
     return lista_peliculas, #url_portada
 
 # Drop-down con nombres de peliculas
@@ -50,26 +51,27 @@ peliculas_seleccionadas = st.selectbox(
     nombres_peliculas
 )
 
+
 if st.button('Mostrar Recomendaciones'):
     libros_recomendados, = recomendar_Pelicula(peliculas_seleccionadas)
     col1, col2, col3, col4, col5 = st.columns(5)
     
     with col1:
-        st.text(libros_recomendados[1])
-        #st.image(url_portada[1])
+        st.text(textwrap.fill(libros_recomendados[0], width=10))
+        #st.image(url_portada[0])
         
     with col2:
-        st.text(libros_recomendados[2])
-        #st.image(url_portada[2])
+        st.text(textwrap.fill(libros_recomendados[1], width=10))
+        #st.image(url_portada[1])
         
     with col3:
-        st.text(libros_recomendados[3])
-        #st.image(url_portada[3])
+        st.text(textwrap.fill(libros_recomendados[2], width=10))  
+        #st.image(url_portada[2])
         
     with col4:
-        st.text(libros_recomendados[4])
-        #st.image(url_portada[4])
+        st.text(textwrap.fill(libros_recomendados[3], width=10))
+        #st.image(url_portada[3])
         
     with col5:
-        st.text(libros_recomendados[5])
-        #st.image(url_portada[5])
+       st.text(textwrap.fill(libros_recomendados[4], width=10))
+        #st.image(url_portada[4])
