403d50012a9f62fb1b09553cc1423b5f3fbc8cc9
kebelth <kebelthcalvo@gmail.com>
2023-11-25 22:40:55 -0600

    portadas

--- aplicacion.py
+++ aplicacion.py
@@ -2,6 +2,7 @@ import pickle
 import textwrap
 
 import numpy as np
+import requests
 import streamlit as st
 
 st.header("Sistema de recomendación de peliculas basado en filtro colavorativo")
@@ -10,25 +11,46 @@ nombres_peliculas = pickle.load(open(r'C:/Users/kebelth/Documents/Filtro_Movies/
 clasificaciones_finales = pickle.load(open(r'C:/Users/kebelth/Documents/Filtro_Movies/activos/clasificaciones_Finales.pkl', 'rb'))
 peliculas_pivot = pickle.load(open(r'C:/Users/kebelth/Documents/Filtro_Movies/activos/peliculas_Tabla_Pivot.pkl', 'rb'))
 
+
 #Función para obtener la portada de la pelicula
 
-def conseguir_portada(sugerencia):
-    nombre_pelicula = []
-    id_indice = []
-    url_portada = []
+
+def obtener_url_imagen(api_key, nombre_pelicula):
+
+    base_url = "https://api.themoviedb.org/3"
+
+    endpoint_busqueda = "/search/movie"
+
+
+    params = {
+        'api_key': api_key,
+        'query': nombre_pelicula
+    }
+
+
+    response = requests.get(f"{base_url}{endpoint_busqueda}", params=params)
+
+    if response.status_code == 200:
+      
+        resultados = response.json()['results']
+
+      
+        if resultados and 'poster_path' in resultados[0]:
+          
+            imagen_path = resultados[0]['poster_path']
+            imagen_url = f"https://image.tmdb.org/t/p/w500{imagen_path}"
+            return imagen_url
+        else:
+            return "https://static.vecteezy.com/system/resources/previews/011/860/696/original/its-movie-time-free-vector.jpg"
     
-    for pelicula_id in sugerencia:
-        nombre_pelicula.append(peliculas_pivot.index[pelicula_id])
-        
-    for nombre in nombre_pelicula[0]:
-        ids = np.where(clasificaciones_finales['Título'] == nombre)[0][0]
-        id_indice.append(ids)
-        
-    for i in id_indice:
-        url = clasificaciones_finales.iloc[i]['Url_imagen']
-        url_portada.append(url)
-        
-    return url_portada
+
+
+clave_de_api = '095ed49a40a37f91383ca3dfdb74618c'
+
+
+
+
+
 
 
 #Función para recomendar pelicula de acuerdo al número de indice del nombre dado como parámetro
@@ -53,25 +75,51 @@ peliculas_seleccionadas = st.selectbox(
 
 
 if st.button('Mostrar Recomendaciones'):
-    libros_recomendados, = recomendar_Pelicula(peliculas_seleccionadas)
+    peliculas_recomendadas, = recomendar_Pelicula(peliculas_seleccionadas)
     col1, col2, col3, col4, col5 = st.columns(5)
-    
+           
     with col1:
-        st.text(textwrap.fill(libros_recomendados[0], width=10))
-        #st.image(url_portada[0])
+        st.text(textwrap.fill(peliculas_recomendadas[0], width=10))
+        nombre_pelicula_a_buscar = peliculas_recomendadas[0]
+        indice_parentesis_apertura = nombre_pelicula_a_buscar.find('(')
+        nombre_pelicula_sin_año = nombre_pelicula_a_buscar[:indice_parentesis_apertura].strip()
+        url_imagen = obtener_url_imagen(clave_de_api, nombre_pelicula_sin_año)
+        st.image(url_imagen)
+        
         
     with col2:
-        st.text(textwrap.fill(libros_recomendados[1], width=10))
-        #st.image(url_portada[1])
+        st.text(textwrap.fill(peliculas_recomendadas[1], width=10))
+        nombre_pelicula_a_buscar = peliculas_recomendadas[1]
+        indice_parentesis_apertura = nombre_pelicula_a_buscar.find('(')
+        nombre_pelicula_sin_año = nombre_pelicula_a_buscar[:indice_parentesis_apertura].strip()
+        url_imagen = obtener_url_imagen(clave_de_api, nombre_pelicula_sin_año)
+        st.image(url_imagen)
+        
         
     with col3:
-        st.text(textwrap.fill(libros_recomendados[2], width=10))  
-        #st.image(url_portada[2])
+        st.text(textwrap.fill(peliculas_recomendadas[2], width=10))  
+        nombre_pelicula_a_buscar = peliculas_recomendadas[2]
+        indice_parentesis_apertura = nombre_pelicula_a_buscar.find('(')
+        nombre_pelicula_sin_año = nombre_pelicula_a_buscar[:indice_parentesis_apertura].strip()
+        url_imagen = obtener_url_imagen(clave_de_api, nombre_pelicula_sin_año)
+        st.image(url_imagen)
+        
         
     with col4:
-        st.text(textwrap.fill(libros_recomendados[3], width=10))
-        #st.image(url_portada[3])
+        st.text(textwrap.fill(peliculas_recomendadas[3], width=10))
+        nombre_pelicula_a_buscar = peliculas_recomendadas[3]
+        indice_parentesis_apertura = nombre_pelicula_a_buscar.find('(')
+        nombre_pelicula_sin_año = nombre_pelicula_a_buscar[:indice_parentesis_apertura].strip()
+        url_imagen = obtener_url_imagen(clave_de_api, nombre_pelicula_sin_año)
+        st.image(url_imagen)
+        
         
     with col5:
-       st.text(textwrap.fill(libros_recomendados[4], width=10))
-        #st.image(url_portada[4])
+        st.text(textwrap.fill(peliculas_recomendadas[4], width=10))
+        nombre_pelicula_a_buscar = peliculas_recomendadas[4]
+        indice_parentesis_apertura = nombre_pelicula_a_buscar.find('(')
+        nombre_pelicula_sin_año = nombre_pelicula_a_buscar[:indice_parentesis_apertura].strip()
+        url_imagen = obtener_url_imagen(clave_de_api, nombre_pelicula_sin_año)
+        st.image(url_imagen)
+        
+  
